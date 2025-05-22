1124f32a21c17c4edf30f8d1f5bcb18d05a0c659
Yagyansh Gupta <147188891+Qureshi02@users.noreply.github.com>
2025-05-22 20:30:43 +0530

    Implement code changes to enhance functionality and improve performance

--- app/src/main/java/com/example/bookworm/utils/Constants.kt
+++ app/src/main/java/com/example/bookworm/utils/Constants.kt
@@ -4,7 +4,7 @@ import com.example.bookworm.BuildConfig
 
 object Constants {
     @Suppress("AuthLeak")
-    const val MONGO_BASE_URL = "http://192.168.36.196:3000/"
+    const val MONGO_BASE_URL = "http://192.168.40.196:3000/"
     const val GOOGLE_BASE_URL = "https://www.googleapis.com/books/v1/"
     fun getApiKey(): String {
         return BuildConfig.API_KEY
